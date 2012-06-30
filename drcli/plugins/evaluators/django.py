3e99bcc42ce43dc34d48623fa01c2354fc96536e
Joel Nothman <joel.nothman@gmail.com>
2012-06-30 21:26:32 +1000

    Django template evaluator

--- drcli/plugins/evaluators/django.py
+++ drcli/plugins/evaluators/django.py
@@ -1,2 +1,26 @@
+try:
+  from django.conf import settings
+  try:
+      settings.configure()
+  except RuntimeError:
+      pass # assume already configured
+  from django.template import Template, Context
+except ImportError:
+  Template = None
 
-# TODO
+from drcli.api import Evaluator
+from drcli.evalargs import STRING_AP
+
+
+class DjangoEvaluator(Evaluator):
+  arg_parsers = (STRING_AP,)
+
+  def __init__(self, argparser, args):
+    self.template = Template(args.string)
+
+  def __call__(self, doc, ind):
+    return self.template.render(Context(dict(doc=doc, ind=ind), autoescape=False))
+
+
+if Template:
+  DjangoEvaluator.register_name('django')

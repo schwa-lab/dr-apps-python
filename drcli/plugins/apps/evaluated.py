b86da32ee9c67c04dad415029814a0db7d9852bf
Joel Nothman <joel.nothman@gmail.com>
2012-07-22 00:47:46 +1000

    Sort app

--- drcli/plugins/apps/evaluated.py
+++ drcli/plugins/apps/evaluated.py
@@ -1,6 +1,8 @@
 """
 Apps which evaluate a function for each doc and act upon the result.
 """
+from StringIO import StringIO
+from schwa import dr
 from drcli.api import App
 from drcli.appargs import DESERIALISE_AP, OSTREAM_AP, get_evaluator_ap
 
@@ -34,5 +36,30 @@ class FilterApp(App):
         writer.write_doc(doc)
 
 
+class SortApp(App):
+  """
+  Sort the documents using an evaluator.
+  """
+  arg_parsers = (get_evaluator_ap(), DESERIALISE_AP, OSTREAM_AP)
+
+  def __call__(self):
+    tmp_out = StringIO()
+    tmp_writer = dr.Writer(tmp_out)
+    evaluator = self.evaluator
+    items = []
+    for i, doc in enumerate(self.stream_reader):
+      # TODO: avoid re-serialising
+      doc_key = evaluator(doc, i)
+      tmp_writer.write_doc(doc)
+      doc_data = tmp_out.getvalue()
+      tmp_out.truncate(0)
+      items.append((doc_key, doc_data))
+
+    items.sort()
+    for doc_key, doc_data in items:
+      print >> self.args.out_stream, doc_data
+
+
 FormatApp.register_name('format')
 FilterApp.register_name('filter')
+SortApp.register_name('sort')

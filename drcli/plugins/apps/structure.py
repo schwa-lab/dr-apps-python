c1e71db4126299781bdf58a6ce60366ac039bc86
Joel Nothman <joel.nothman@gmail.com>
2012-07-22 00:29:05 +1000

    dr select and rename app to change doc fields

--- drcli/plugins/apps/structure.py
+++ drcli/plugins/apps/structure.py
@@ -1,24 +1,136 @@
 """
 Apps to restructure a corpus.
 """
+from collections import defaultdict
 from itertools import izip
 from drcli.api import App
 from drcli.appargs import DESERIALISE_AP, OSTREAM_AP, ISTREAM_AP, ArgumentParser
+
+
+def SelectField(s):
+  # TODO: ensure valid variable names
+  parts = tuple(s.split('.'))
+  if len(parts) == 1:
+    return (None,) + parts
+  if len(parts) == 2:
+    return parts
+  raise ValueError('Maximum one . may appear in fields')
+
 
 class SelectApp(App):
   """
   Select only (or remove) specified fields on each document.
   """
-  arg_parsers = (DESERIALISE_AP, OSTREAM_AP)
-  pass
-  # TODO: can this be done in Python API?
+  field_list_ap = ArgumentParser()
+  field_list_ap.add_argument('fields', nargs='+', type=SelectField, help='Fields or stores to include (or exclude with -x). These are attributes on the document by default. When taking the form Class.field, Class objects will be similarly processed to retain or exclude given fields.')
+  field_list_ap.add_argument('-x', '--exclude', action='store_true', default=False, help='Treat all fields listed as those to exclude rather than to retain.')
+  arg_parsers = (field_list_ap, ISTREAM_AP, OSTREAM_AP)
+
+  def __init__(self, argparser, args):
+    field_dict = defaultdict(set)
+    for klass, field in (args.fields or ()):
+      field_dict[klass].add(field)
+    args.doc_fields = field_dict[None]
+    args.annot_fields = dict(field_dict)
+    if args.exclude:
+      self._perform = self._perform_exclude
+    else:
+      self._perform = self._perform_select
+    super(SelectApp, self).__init__(argparser, args)
+
+  def __call__(self):
+    # FIXME: externalise reflection methods ... or avoid it by just deleting attributes
+    writer = self.stream_writer
+    for doc in self.stream_reader:
+      for store in doc._dr_stores.itervalues():
+        try:
+          fields = self.args.annot_fields[store.klass_name]
+        except KeyError:
+          continue
+        self._perform(fields, store._klass._dr_s2p, store._klass._dr_fields)
+
+      if self.args.doc_fields:
+        self._perform(self.args.doc_fields, doc._dr_s2p, doc._dr_fields, doc._dr_stores)
+      writer.write_doc(doc)
+
+  def _perform_exclude(self, fields, *attr_dicts):
+    # FIXME: work for non-identity s2p maps, if necessary
+    for attr_dict in attr_dicts:
+      for f in fields:
+        try:
+          del attr_dict[f]
+        except KeyError:
+          pass
+
+  def _perform_select(self, fields, *attr_dicts):
+    # FIXME: work for non-identity s2p maps, if necessary
+    for attr_dict in attr_dicts:
+      for f in set(attr_dict) - fields:
+        try:
+          del attr_dict[f]
+        except KeyError:
+          pass
+
+
+def RenameField(s):
+  if s.count('=') != 1:
+    raise ValueError('Argument must contain exactly one =')
+  new, old = s.split('=')
+  klass, new = SelectField(new)
+  return klass, new, old
 
 
 class RenameApp(App):
   """
   Rename specified fields or stores.
   """
-  arg_parsers = (ISTREAM_AP, OSTREAM_AP)
+  # TODO: rename annotation classes
+  rename_list_ap = ArgumentParser()
+  rename_list_ap.add_argument('renames', nargs='+', type=RenameField, help='Rename description of form [Class.]new_name=old_name')
+  arg_parsers = (rename_list_ap, ISTREAM_AP, OSTREAM_AP)
+
+  def __init__(self, argparser, args):
+    rename_dict = defaultdict(set)
+    for klass, new, old in (args.renames or ()):
+      rename_dict[klass].add((new, old))
+    args.renames = dict(rename_dict)
+    super(RenameApp, self).__init__(argparser, args)
+
+  def __call__(self):
+    # FIXME: externalise reflection methods
+    writer = self.stream_writer
+    for doc in self.stream_reader:
+      classes = {None: doc.__class__}
+      classes.update((store.klass_name, store._klass) for store in doc._dr_stores.itervalues())
+      for klass_name, klass in classes.iteritems():
+        try:
+          renames = self.args.renames[klass_name]
+        except KeyError:
+          continue
+
+        relevant = []
+        for new, old in renames:
+          try:
+            del klass._dr_s2p[old]
+          except KeyError:
+            pass
+          else:
+            relevant.append((new, old))
+        # s2p isn't used in Writer at present, but we'll update it just in case
+        klass._dr_s2p.update(relevant)
+
+        fields = klass._dr_fields.copy()
+        fields.update(getattr(klass, '_dr_stores', ()))
+        for new, old in relevant:
+          fields[old].serial = new
+
+      writer.write_doc(doc)
+
+
+class Compose(App):
+  """
+  Given two document streams of equal length, output their pairwise composition.
+  """
   pass
   # TODO: can this be done in Python API?
 
@@ -61,4 +173,6 @@ class HeadApp(App):
       writer.write_doc(doc)
 
 
+SelectApp.register_name('select')
+RenameApp.register_name('rename')
 HeadApp.register_name('head')

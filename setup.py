41416afdd05df56180a8d795ed77e83e0851efdd
Joel Nothman <joel.nothman@gmail.com>
2012-09-13 14:26:25 +1000

    Set version to 0.2; add brownie dependency

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 from setuptools import setup
 
 setup(name='drcli',
-      version='0.1',
+      version='0.2',
       description='Command-line tools for docrep',
       author = 'schwa lab',
       url='http://schwa.org/git/drcli.git',
       packages=['drcli'],
       scripts=['dr'],
-      install_requires=['argparse'],
+      install_requires=['argparse', 'brownie'],
       package_data = {
         'drcli': ['plugins/*/*.py'],
       },

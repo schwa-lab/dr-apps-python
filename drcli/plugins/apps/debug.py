788d85bf9aa6e109dd27554e7d684b126d99c94e
Joel Nothman <joel.nothman@gmail.com>
2012-10-07 17:41:44 +1100

    An application to hack changes to doc headers

--- drcli/plugins/apps/debug.py
+++ drcli/plugins/apps/debug.py
@@ -1,7 +1,7 @@
 
 import msgpack
 import pprint
-import json
+import ast
 from schwa import dr
 from schwa.dr.constants import FIELD_TYPE_NAME, FIELD_TYPE_POINTER_TO, FIELD_TYPE_IS_SLICE
 from drcli.api import App
@@ -78,4 +78,110 @@ class DumpApp(App):
       yield name, field
 
 
+class HackHeaderApp(App):
+  """
+  Debug: rewrite header components of given documents using Python literal input
+  """
+  hack_ap = ArgumentParser()
+  hack_ap.add_argument('--klasses', default=None, help='Overwrites the entire klasses header with the given list')
+  hack_ap.add_argument('-k', '--klass', default=[], action='append', help='Overwrites a klass definition, specified with <name|num>=[<new_name>,<field list>]')
+  hack_ap.add_argument('-f', '--field', default=[], action='append', help='Overwrites a field definition, specified with <klass-name|num>.<field-name|num>[+]=<map> (use += for update semantics)')
+  arg_parsers = (hack_ap, ISTREAM_AP, OSTREAM_AP)
+
+  def __init__(self, argparser, args):
+    super(HackHeaderApp, self).__init__(argparser, args)
+
+    def parse(s, exp_type):
+      try:
+        res = ast.literal_eval(s)
+      except (SyntaxError, ValueError):
+        argparser.error('{0} is not a valid Python literal'.format(s))
+      if exp_type is not None and type(res) != exp_type:
+        argparser.error('{0} does not evaluate to type {1}'.format(s, exp_type))
+      return res
+    
+    self.operations = []
+
+    if args.klasses:
+      self.operations.append((self._set_klasses, {'value': parse(args.klasses, list)}))
+
+    for arg in args.klass:
+      try:
+        key, value = arg.split('=', 1)
+      except ValueError:
+        argparser.error('Expected <name>=<value>, got {0}'.format(arg))
+      try:
+        key = int(key)
+      except ValueError:
+        pass
+      value = parse(value, list)
+      if len(value) != 2:
+        argparser.error('Expected a list of length 2, got {0}'.format(value))
+      self.operations.append((self._set_klass, {'klass': key, 'value': value}))
+
+    for arg in args.field:
+      try:
+        key, value = arg.split('=', 1)
+        kname, fname = key.split('.')
+      except ValueError:
+        argparser.error('Expected <kname>.<fname>=<value>, got {0}'.format(arg))
+      if fname.endswith('+'):
+        fname = fname[:-1]
+        update = True
+      else:
+        update = False
+      try:
+        kname = int(kname)
+      except ValueError:
+        pass
+      try:
+        fname = int(fname)
+      except ValueError:
+        pass
+      value = parse(value, dict)
+      self.operations.append((self._set_field, {'klass': kname, 'field': fname, 'value': value, 'update': update}))
+    
+    if not self.operations:
+      argparser.error('Nothing to do!')
+
+  def _set_klasses(self, klasses, stores, value):
+    klasses[:] = value
+
+  def _set_klass(self, klasses, stores, klass, value):
+    if klass == len(klasses):
+      klasses.append(value)
+    for knum, (kname, fields) in enumerate(klasses):
+      if klass in (knum, kname):
+        klasses[knum] = value
+        return
+    raise ValueError('Could not find class {0}'.format(klass))
+
+  def _set_field(self, klasses, stores, klass, field, value, update=False):
+    for knum, (kname, fields) in enumerate(klasses):
+      if klass not in (knum, kname):
+        continue
+
+      if field == len(fields):
+        fields.append({})
+
+      for fnum, fdef in enumerate(fields):
+        fname = fdef.get(FIELD_TYPE_NAME)
+        if field in (fnum, fname):
+          if update:
+            fields[fnum].update(value)
+          else:
+            fields[fnum] = value
+          return
+
+    raise ValueError('Could not find field {1} in class {0}'.format(klass, field))
+  
+  def __call__(self):
+    writer = self.raw_stream_writer
+    for doc in self.raw_stream_reader:
+      for fn, kwargs in self.operations:
+        fn(doc.klasses, doc.stores, **kwargs)
+      writer.write(doc)
+
+
 DumpApp.register_name('dump')
+HackHeaderApp.register_name('hackheader')

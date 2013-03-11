1c3f089a1f0ca11193ca76f37cc197d9ea82d201
Joel Nothman <joel.nothman@gmail.com>
2013-03-11 19:58:05 +1100

    Indent -> 2 spaces

--- drcli/util.py
+++ drcli/util.py
@@ -4,39 +4,39 @@ import msgpack
 RawDoc = namedtuple('RawDoc', ('version', 'klasses', 'stores', 'doc', 'instances'))
 
 def read_raw_docs(unpacker):
-    if not hasattr(unpacker, 'unpack'):
-        unpacker = msgpack.Unpacker(unpacker, use_list=True)
-    unpack = unpacker.unpack
-    while True:
-        try:
-            klasses = unpack()
-        except msgpack.OutOfData:
-            return
-        if isinstance(klasses, int):
-            version = klasses
-            klasses = unpack()
-        else:
-            version = 1
-        stores = unpack()
-        yield RawDoc(version, klasses, stores, unpack() and unpack(), [unpack() and unpack() for i in range(len(stores))])
+  if not hasattr(unpacker, 'unpack'):
+    unpacker = msgpack.Unpacker(unpacker, use_list=True)
+  unpack = unpacker.unpack
+  while True:
+    try:
+      klasses = unpack()
+    except msgpack.OutOfData:
+      return
+    if isinstance(klasses, int):
+      version = klasses
+      klasses = unpack()
+    else:
+      version = 1
+    stores = unpack()
+    yield RawDoc(version, klasses, stores, unpack() and unpack(), [unpack() and unpack() for i in range(len(stores))])
 
 def write_with_nbytes(out, obj):
-    packed = msgpack.packb(obj)
-    msgpack.pack(len(packed), out)
-    out.write(packed)
+  packed = msgpack.packb(obj)
+  msgpack.pack(len(packed), out)
+  out.write(packed)
 
 def write_raw_doc(out, doc):
-    if doc.version != 1:
-        msgpack.pack(doc.version, out)
-    msgpack.pack(doc.klasses, out)
-    msgpack.pack(doc.stores, out)
-    write_with_nbytes(out, doc.doc)
-    for insts in doc.instances:
-        write_with_nbytes(out, insts)
+  if doc.version != 1:
+    msgpack.pack(doc.version, out)
+  msgpack.pack(doc.klasses, out)
+  msgpack.pack(doc.stores, out)
+  write_with_nbytes(out, doc.doc)
+  for insts in doc.instances:
+    write_with_nbytes(out, insts)
 
 class RawDocWriter(object):
-    def __init__(self, out):
-        self.out = out
+  def __init__(self, out):
+    self.out = out
 
-    def write(self, doc):
-        write_raw_doc(self.out, doc)
+  def write(self, doc):
+    write_raw_doc(self.out, doc)
